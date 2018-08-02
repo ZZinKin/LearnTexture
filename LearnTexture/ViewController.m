ed8b3d7b2a663f90e8bbb773bc573f51d490eb4b
Chaolong <liao.chaolong.sz@gmail.com>
2018-08-02 23:57:22 +0800

    Initial Commit

---
+++ LearnTexture/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LearnTexture
+//
+//  Created by cello on 2018/8/2.
+//  Copyright © 2018年 ZZinKin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ LearnTexture/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LearnTexture
+//
+//  Created by cello on 2018/8/2.
+//  Copyright © 2018年 ZZinKin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
